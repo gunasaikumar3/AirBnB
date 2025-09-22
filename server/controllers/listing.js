6fdd6cfa832e86acdc50607888d8cf5f76166690
gunasaikumar3 <gunasaikumar7920@gmail.com>
2025-09-22 10:34:16 +0530

    added contollers

---
+++ server/controllers/listing.js
@@ -0,0 +1,39 @@
+const Listing = require("../models/listing.js");
+
+module.exports.index = async (req, res) => {
+  const allListings = await Listing.find({});
+  res.send(allListings);
+};
+
+module.exports.show = async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.send(listing);
+};
+
+module.exports.create = async (req, res) => {
+  let newListing = new Listing(req.body.listing);
+  await newListing.save();
+
+  res.status(201).json({
+    message: "Listing created successfully",
+  });
+};
+
+module.exports.update = async (req, res) => {
+  const { id } = req.params;
+
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.status(201).json({
+    message: "Listing created successfully",
+  });
+};
+
+module.exports.delete = async (req, res) => {
+  const { id } = req.params;
+
+  await Listing.findByIdAndDelete(id);
+  res.status(201).json({
+    message: "Listing deleted successfully",
+  });
+};
--- server/routes/listing.js
+++ server/routes/listing.js
@@ -5,8 +5,7 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
 
 const { listingSchema } = require("../schema.js");
-
-const Listing = require("../models/listing.js");
+const listingController = require("../controllers/listing.js");
 
 const validateListing = (req, res, next) => {
   const error = listingSchema.validate(req.body);
@@ -20,60 +19,18 @@ const validateListing = (req, res, next) => {
 };
 
 // Index Route
-router.get("/", async (req, res) => {
-  const allListings = await Listing.find({});
-  res.send(allListings);
-});
+router.get("/", listingController.index);
 
 //Show Route
-router.get(
-  "/:id",
-  wrapAsync(async (req, res) => {
-    const { id } = req.params;
-    const listing = await Listing.findById(id);
-    res.send(listing);
-  })
-);
+router.get("/:id", wrapAsync(listingController.show));
 
 //Create Route
-router.post(
-  "/",
-  validateListing,
-  wrapAsync(async (req, res) => {
-    let newListing = new Listing(req.body.listing);
-    await newListing.save();
-
-    res.status(201).json({
-      message: "Listing created successfully",
-    });
-  })
-);
+router.post("/", validateListing, wrapAsync(listingController.create));
 
 //Update Route
-router.put(
-  "/:id",
-  validateListing,
-  wrapAsync(async (req, res) => {
-    const { id } = req.params;
-
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-    res.status(201).json({
-      message: "Listing created successfully",
-    });
-  })
-);
+router.put("/:id", validateListing, wrapAsync(listingController.update));
 
 //Delete Route
-router.delete(
-  "/:id",
-  wrapAsync(async (req, res) => {
-    const { id } = req.params;
-
-    await Listing.findByIdAndDelete(id);
-    res.status(201).json({
-      message: "Listing deleted successfully",
-    });
-  })
-);
+router.delete("/:id", wrapAsync(listingController.delete));
 
 module.exports = router;
